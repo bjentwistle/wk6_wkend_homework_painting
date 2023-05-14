129d5d78dfde033dd744f44eedc6dd60d8e27776
Becky <bjentwistle@gmail.com>
2023-05-14 09:44:35 +0100

    managed to correct uppercase function

--- uppercase/upper_caser.js
+++ uppercase/upper_caser.js
@@ -1,7 +1,9 @@
 const UpperCaser = function (words) {
+    this.words = words
 }
 
 UpperCaser.prototype.toUpperCase = function () {
+    return this.words.map(word => word.toLocaleUpperCase('en-US'))
 
 }
 
